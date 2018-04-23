b54bf4825f0d2a434bd2a9db8a6ff13af94f6fb3
J. R. Matchett <jrmatchett@me.com>
2018-04-23 12:41:36 -0700

    Specified interal extras objects.

--- extras.py
+++ extras.py
@@ -1,9 +1,9 @@
 """Various functions for working with GIS data."""
 
-import os
-import urllib
-from mgrspy import mgrs
-import numpy
+import os as _os
+import urllib as _urllib
+from mgrspy import mgrs as _mgrs
+import numpy as _numpy
 
 
 def round_significant(x, p=2):
@@ -11,14 +11,14 @@ def round_significant(x, p=2):
 
     Arguments:
     x  A numeric value.
-    p  Number of significant digits.
+    p  Significant digits precision.
     """
     if x == 0.0:
         return x
     elif x < 0:
         raise ValueError('Value must be positive.')
     else:
-        return numpy.around(x, -int(numpy.floor(numpy.log10(x))) + (p - 1))
+        return _numpy.around(x, -int(_numpy.floor(_numpy.log10(x))) + (p - 1))
 
 
 def fix_fgdb_files(dir):
@@ -29,9 +29,9 @@ def fix_fgdb_files(dir):
     Arguments:
     dir  Directory containing the geodatabase files.
     """
-    os.chdir(dir)
-    for file in os.listdir():
-        os.rename(file, file.split('\\')[1])
+    _os.chdir(dir)
+    for file in _os.listdir():
+        _os.rename(file, file.split('\\')[1])
 
 
 def google_maps_url(latitude, longitude):
@@ -54,7 +54,7 @@ def apple_maps_url(latitude, longitude, label='X'):
     label      Text label for map point.
     """
     return 'http://maps.apple.com/?ll={0:2.5f},{1:3.5f}&q={2:s}'.format(
-        latitude, longitude, urllib.parse.quote(label))
+        latitude, longitude, _urllib.parse.quote(label))
 
 
 def usng(latitude, longitude, precision=4):
@@ -65,7 +65,7 @@ def usng(latitude, longitude, precision=4):
     longitude  Longitude in decimal degrees.
     precision  Grid value precision.
     """
-    usng = mgrs.toMgrs(latitude, longitude, precision)
+    usng = _mgrs.toMgrs(latitude, longitude, precision)
     usng_fmt = []
     usng_fmt.append(usng[0:3])
     usng_fmt.append(usng[3:5])
